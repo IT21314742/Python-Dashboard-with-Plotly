4fed9a95931cf774ef5c690e35cc1c3e15cf8d04
IT21314742 <it21314742@my.sliit.lk>
2025-06-23 12:03:04 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -24,6 +24,5 @@ app.layout = dbc.Container([
         dbc.Col(html.H1("Healthcare Dashboard"), width=15, className="text-center my-5")
     ])
 ])
-
 if __name__ == "__main__":
     app.run_server(debug=True)
