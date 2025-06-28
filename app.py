06f0614b78bcd788e6b68f6f0843f91c686921df
IT21314742 <it21314742@my.sliit.lk>
2025-06-28 19:48:56 +0530

    Update
    Update

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ app.layout = dbc.Container([
     ])
 ])
 
-                                    
+                                        
 
 if __name__ == "__main__":
     app.run_server(debug=True)
